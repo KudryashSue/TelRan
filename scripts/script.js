301b665e1018f029c08c0b0a114b01b3b12de537
Елена Васильева <kudryashsue@gmail.com>
2022-06-29 21:50:45 +0200

    first

---
+++ scripts/script.js
@@ -0,0 +1,16 @@
+// .reduce() - служит для того, чтобы мы получили какое-то финальное значение, совершив итерацию по массиву. Колбэк принимает в себя два параметра accumulator (currentValue, previousValue, total) и итерируемый элемент (nextValue).
+
+/*let arr = [1, 2, 3, 4, 5];
+
+let sum = arr.reduce((total, el) => total + el);
+
+console.log(sum);*/
+
+//1. Найти среднеарифметическое число
+
+let arr = [33, 54, 12, 1, 4];
+
+let sum = arr.reduce((total, el) => total + el)/arr.length;
+
+console.log(sum);
+
